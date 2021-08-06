a8b01ce1a0bb2f265d2b4205c97ac880aa057e8b
Katte <tacocatytreal@gmail.com>
2021-08-05 23:52:43 -0700

    f

---
+++ TreeBreaking.gd
@@ -0,0 +1,28 @@
+extends StaticBody
+const WoodPickup = preload("res://WoodPickup.tscn")
+
+# Declare member variables here. Examples:
+var health
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#var anim = $AnimationPlayer
+	health = 15
+	randomize()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area_area_entered(area):
+	$AnimationPlayer.play("Tree_Hit")
+	health -= 15
+	if health < 1:
+		var dropCount = round(rand_range(1, 5))
+		while dropCount > 0:
+			var wood_pickup = WoodPickup.instance()
+			wood_pickup.transform.origin = transform.origin
+			wood_pickup.set_linear_velocity(Vector3(rand_range(-5,5), rand_range(1,5), rand_range(-5,5)))
+			get_parent().add_child(wood_pickup)
+			dropCount -= 1
+		queue_free()
